9903a7a72e9388e631b4547aa44ad364de467a5a
circulodehierro <120426643+circulodehierro@users.noreply.github.com>
2023-03-02 20:46:55 -0300

    Add files via upload

--- js/efectos.js
+++ js/efectos.js
@@ -1,3 +1,11 @@
+$(document).ready(function () {
+    function reorient(e) {
+      var portrait = (window.orientation % 180 == 0);
+      $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+    }
+    window.onorientationchange = reorient;
+    window.setTimeout(reorient, 0);
+  });
 function logo(){
     document.getElementById("general").style.display = 'none';
     document.getElementById("eventos").style.display = 'none';
