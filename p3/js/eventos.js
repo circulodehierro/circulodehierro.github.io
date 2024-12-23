de2cb7fe8dc6cd961261a49f60e91a56ade775ea
Robinson Gonzalez <rgonzalez@ifk.cl>
2024-12-23 03:56:07 -0300

    t

--- p3/js/eventos.js
+++ p3/js/eventos.js
@@ -1,21 +1,3 @@
-// Función para activar la pantalla completa
-function goFullScreen() {
-    let docEl = document.documentElement;
-
-    if (docEl.requestFullscreen) {
-        docEl.requestFullscreen();
-    } else if (docEl.mozRequestFullScreen) { // Firefox
-        docEl.mozRequestFullScreen();
-    } else if (docEl.webkitRequestFullscreen) { // Chrome, Safari and Opera
-        docEl.webkitRequestFullscreen();
-    } else if (docEl.msRequestFullscreen) { // IE/Edge
-        docEl.msRequestFullscreen();
-    }
-}
-
-// Intentar entrar en pantalla completa cuando se carga la página
-window.onload = goFullScreen;
-
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.row.header');
 
@@ -26,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let currentYear = today.getFullYear();
         const currentDay = today.getDate();
 
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 5; i++) {
             const table = document.createElement('table');
             table.classList.add('month-table');
 
