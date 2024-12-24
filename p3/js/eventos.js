bb13614508a0b7be91413c88f350e7d9571e4649
Robinson Gonzalez <rgonzalez@ifk.cl>
2024-12-24 02:08:55 -0300

    g

--- p3/js/eventos.js
+++ p3/js/eventos.js
@@ -3,13 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const content = document.querySelector('.row.content'); // El contenedor donde se mostrarán los detalles del evento
 
     const eventos = [
-        { ano: 2024, mes: 12, dia: 25, evento: 'Navidad', lugar: 'Casa', imagen: '../images/navidad.jpg', detalle: 'Celebración de Navidad en familia.' },
-        { ano: 2025, mes: 1, dia: 1, evento: 'Año Nuevo', lugar: 'Plaza Central', imagen: '../images/ano_nuevo.jpg', detalle: 'Fiesta para recibir el Año Nuevo.' },
-        { ano: 2025, mes: 1, dia: 12, evento: 'Especial', lugar: 'Casa', imagen: '../images/especial.jpg', detalle: 'Evento especial con amigos.' },
-        { ano: 2025, mes: 1, dia: 24, evento: 'Ruta Anual C.D.H.M.', lugar: 'Serena', imagen: '../images/evento.jpeg', detalle: 'Ruta Anual Larga del Circulo de Hierro Motoclub.' },
-        { ano: 2025, mes: 1, dia: 25, evento: 'Ruta Anual C.D.H.M.', lugar: 'Serena', imagen: '../images/evento.jpeg', detalle: 'Ruta Anual Larga del Circulo de Hierro Motoclub.' },
-        { ano: 2025, mes: 1, dia: 26, evento: 'Ruta Anual C.D.H.M.', lugar: 'Serena', imagen: '../images/evento.jpeg', detalle: 'Ruta Anual Larga del Circulo de Hierro Motoclub.' },
-        { ano: 2025, mes: 2, dia: 14, evento: 'San Valentín', lugar: 'Restaurante', imagen: '../images/san_valentin.jpg', detalle: 'Cena romántica en restaurante.' }
+        { ano: 2025, mes: 1, dia: 10, duracion: '10, 11 y 12 de Enero', evento: 'Segundo Encuentro', lugar: 'Fundo el Porvenir', imagen: '../images/segundo_encuentro.png', detalle: 'Sector Quiriquina, Camino Calle Alegre KM 3,5' },
+        { ano: 2025, mes: 1, dia: 11, duracion: '10, 11 y 12 de Enero', evento: 'Segundo Encuentro', lugar: 'Fundo el Porvenir', imagen: '../images/segundo_encuentro.png', detalle: 'Sector Quiriquina, Camino Calle Alegre KM 3,5' },
+        { ano: 2025, mes: 1, dia: 12, duracion: '10, 11 y 12 de Enero', evento: 'Segundo Encuentro', lugar: 'Fundo el Porvenir', imagen: '../images/segundo_encuentro.png', detalle: 'Sector Quiriquina, Camino Calle Alegre KM 3,5' },
+        { ano: 2025, mes: 1, dia: 17, duracion: '17, 18 y 19 de Enero', evento: 'Tercer MotoEncuentro', lugar: 'Valle del Elqui', imagen: '../images/3_motoencuentro.png', detalle: 'Entrada General $25.000' },
+        { ano: 2025, mes: 1, dia: 18, duracion: '17, 18 y 19 de Enero', evento: 'Tercer MotoEncuentro', lugar: 'Valle del Elqui', imagen: '../images/3_motoencuentro.png', detalle: 'Entrada General $25.000' },
+        { ano: 2025, mes: 1, dia: 19, duracion: '17, 18 y 19 de Enero', evento: 'Tercer MotoEncuentro', lugar: 'Valle del Elqui', imagen: '../images/3_motoencuentro.png', detalle: 'Entrada General $25.000' },
+        { ano: 2025, mes: 1, dia: 24, duracion: '24, 25 y 26 de Enero', evento: 'Ruta Anual C.D.H.M.', lugar: 'Serena', imagen: '../images/ruta_circulo.png', detalle: 'Ruta Anual Larga del Circulo de Hierro Motoclub.' },
+        { ano: 2025, mes: 1, dia: 25, duracion: '24, 25 y 26 de Enero', evento: 'Ruta Anual C.D.H.M.', lugar: 'Serena', imagen: '../images/ruta_circulo.png', detalle: 'Ruta Anual Larga del Circulo de Hierro Motoclub.' },
+        { ano: 2025, mes: 1, dia: 26, duracion: '24, 25 y 26 de Enero', evento: 'Ruta Anual C.D.H.M.', lugar: 'Serena', imagen: '../images/ruta_circulo.png', detalle: 'Ruta Anual Larga del Circulo de Hierro Motoclub.' },
+        { ano: 2025, mes: 1, dia: 31, duracion: '31, 01 y 02 de Enero, Febrero', evento: '17 Aniversario', lugar: 'Chiloe', imagen: '../images/piratas_isla.png', detalle: 'Ruta hasta Castro Chiloe.' },
+        { ano: 2025, mes: 2, dia: 1, duracion: '31, 01 y 02 de Enero, Febrero', evento: '17 Aniversario', lugar: 'Chiloe', imagen: '../images/piratas_isla.png', detalle: 'Ruta hasta Castro Chiloe.' },
+        { ano: 2025, mes: 2, dia: 2, duracion: '31, 01 y 02 de Enero, Febrero', evento: '17 Aniversario', lugar: 'Chiloe', imagen: '../images/piratas_isla.png', detalle: 'Ruta hasta Castro Chiloe.' },
+        { ano: 2025, mes: 2, dia: 14, duracion: '14, 15 y 16 de Febrero', evento: '9 Aniversario Mandinga', lugar: 'Camino las Molinas 1156, Chimbarongo', imagen: '../images/a_mandinga2025.png', detalle: 'Entrada General $20.000' },
+        { ano: 2025, mes: 2, dia: 15, duracion: '14, 15 y 16 de Febrero', evento: '9 Aniversario Mandinga', lugar: 'Camino las Molinas 1156, Chimbarongo', imagen: '../images/a_mandinga2025.png', detalle: 'Entrada General $20.000' },
+        { ano: 2025, mes: 2, dia: 16, duracion: '14, 15 y 16 de Febrero', evento: '9 Aniversario Mandinga', lugar: 'Camino las Molinas 1156, Chimbarongo', imagen: '../images/a_mandinga2025.png', detalle: 'Entrada General $20.000' },
     ];
 
     function createCalendars() {
@@ -164,6 +172,19 @@ document.addEventListener('DOMContentLoaded', () => {
         detalles.textContent = evento.detalle;
         tarjeta.appendChild(detalles);
 
+        // Crear un div para el destino
+        const destinoDiv = document.createElement('div');
+        destinoDiv.textContent = `Destino: ${evento.lugar}`; // Añadir el texto con el destino
+        destinoDiv.style.marginTop = '10px'; // Añadir un pequeño margen arriba para separación
+        destinoDiv.style.fontWeight = 'bold'; // Opcional: Negrita para resaltar
+        tarjeta.appendChild(destinoDiv); // Añadir el div a la tarjeta
+
+        // Crear un div para el destino
+        const duracionDiv = document.createElement('div');
+        duracionDiv.textContent = `Duracion: ${evento.duracion}`; // Añadir el texto con el destino
+        duracionDiv.style.marginTop = '10px'; // Añadir un pequeño margen arriba para separación
+        duracionDiv.style.fontWeight = 'bold'; // Opcional: Negrita para resaltar
+        tarjeta.appendChild(duracionDiv); // Añadir el div a la tarjeta
         // Añadir la tarjeta al div content
         content.appendChild(tarjeta);
     }
