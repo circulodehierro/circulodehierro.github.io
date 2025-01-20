2eee485d9ed5f5a40ae76f1899f933513c63e051
Robinson Gonzalez <rgonzalez@ifk.cl>
2025-01-20 05:20:42 -0300

    c

--- p3/js/documentos.js
+++ p3/js/documentos.js
@@ -3,3 +3,18 @@ window.onload = function() {
     // Desplazamos el contenedor al principio
     iconContainer.scrollLeft = 0;
 };
+function habilitar_div(permiso) {
+    const divs = document.querySelectorAll('.icon-item');
+    // Iterar sobre cada div
+    divs.forEach(div => {
+        // Obtener el valor del atributo data-value
+        const value = parseInt(div.getAttribute('data-value')); 
+        // Si el valor no está en el array permiso
+        if (!permiso.includes(value)) {
+            // Añadir la clase 'disabled' al div
+            div.classList.add('disabled');
+            // Evitar el clic en el div deshabilitado
+            div.onclick = (e) => e.preventDefault(); // Opcional para evitar acciones adicionales
+        }
+    });
+}
